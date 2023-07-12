6be5c6dfd04cc6a2a6f9d4327eb872c54e5c6907
iamwebartdevteam <biswajit@webart.technology>
2023-07-12 14:00:11 +0530

    images link upadte

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,6 +1,6 @@
 export const CURRENCY = "$";
 export const TIMEZONE = "America/Los_Angeles";
-export const IMG = "https://wrappedinmusic.webbersunited.com/";
+export const IMG = "https://wrappedinmusic.webbersunited.com/backend";
 // ? ====== BASE URL ======
 export const URL = "https://wrappedinmusic.webbersunited.com:3001/api";
 //export const URL = "http://admin.wrappedinmusic.mypickmyvote.com/api";
